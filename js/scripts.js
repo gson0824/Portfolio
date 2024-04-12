7ed1a37b83f00991aad1b8d2ce2476ee99a27d24
gson0824 <128443982+gson0824@users.noreply.github.com>
2024-04-12 04:26:42 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// const toggleButton = document.getElementById('toggle-button');
+
+
+// const switchTheme = () => {
+//     const isDarkTheme = document.body.classList.toggle('dark-theme');
+      
+//     toggleButton.textContent = isDarkTheme ? 'Light Mode' : 'Dark Mode';
+      
+//     localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+// };
+  
+
+// toggleButton.addEventListener('click', switchTheme);
+
+// const currentTheme = localStorage.getItem('theme');
+
+// if (currentTheme) {
+//     document.body.classList.add(currentTheme);
+
+//     if (currentTheme === 'dark') {
+//         document.body.classList.add('dark-theme');
+//         toggleButton.textContent = 'Light Mode';
+//     }
+
+//     else {
+//         document.body.classList.remove('dark-theme');
+//         toggleButton.textContent = 'Dark Mode';
+//     }
+// }
